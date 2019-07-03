15defa643b1317d8c85139169e83314e8484cbe8
Anna Yensie Topping <47766564+annayensietopping@users.noreply.github.com>
2019-07-03 15:44:40 -0600

    removing js color code

--- src/index.js
+++ src/index.js
@@ -24,13 +24,14 @@ function data(data) {
   $("header").attr("background-image", data.urls.regular);
 }
 
-$(function() {
-  var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+// $(function() {
+//   //var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+//   var randomColor = Math.floor(Math.random() * 8388607 + 4194303).toString(16);
 
-  $("footer").css({
-    backgroundColor: "#" + randomColor
-  });
-  $("header").css({
-    backgroundColor: "#" + randomColor
-  });
-});
+//   $("footer").css({
+//     backgroundColor: "#" + randomColor
+//   });
+//   $("header").css({
+//     backgroundColor: "#" + randomColor
+//   });
+// });

6822927f4f93d3460475e936616537d0163eaad5
Anna Yensie Topping <47766564+annayensietopping@users.noreply.github.com>
2019-08-20 21:24:25 -0400

    wiggly line under links

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import "./styles.css";
 import { default as $ } from "jquery";
 
-// inspo
-// http://seanhalpin.io/
-
-// add click event to hire me that reveals email address in addition to sendto link
-
-$(document).ready(function() {
-  $("#footer-hover").click(function() {
-    $("#footer-reveal").slideToggle(300);
-  });
-});
-
-// random img generator NYC
-$.get(
-  "https://api.unsplash.com/photos/random?client_id=e28892e82256b02ca943a7a7bdb463bfdd5f059fd53ba7b67ea815fcde037557&query=nyc"
-).then(res => {
-  console.log(res);
-  data(res);
-});
-
 function data(data) {
   $("header").attr("background-image", data.urls.regular);
 }
